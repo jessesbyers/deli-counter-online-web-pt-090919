4ff7e04fb96a3bfd9d25ce8ddd94969332429b8b
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-11 23:48:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require 'pry'
+
+#not passing yet - only puts first in line, not entire line
+def line(array)
+  deli_line = []
+  if array.length == 0
+    puts "The line is currently empty."
+
+else
+  deli_line << "The line is currently: "
+    array.each do |name, number|
+    number = array.index + 1
+      line = "#{number}. #{name}"
+      deli_line << "#{line}"
+
+
+end
+    end
+    return deli_line
+  end
+
+
+
+
+def take_a_number(array, string)
+  array << string
+end
