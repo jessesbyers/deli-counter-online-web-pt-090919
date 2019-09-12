c35cc348dc2b1aa3d6e1c12231f1e7cce81d0291
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-12 01:04:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,5 @@
 #require 'pry'
 
-# def line(array)
-#   new_array = []
-#   if array.length == 0
-#      puts "The line is currently empty."
-#    else
-#      number = array.index + 1
-#      array.each do |name, number|
-#      array << "#{number}"
-#      array << "The line is currently: "
-#      new_array << array.join("(#{number} += 1)")
-#   end
-# end
-# new_array
-# end
-
-#not passing yet - only puts first in line, not entire line
 def line(array)
   deli_line = []
   if array.length == 0
@@ -29,8 +13,6 @@ else
   end
 end
 
-
-
-#def take_a_number(array, string)
-#  array << string
-#end
+def take_a_number(array, string)
+ array << string
+end
