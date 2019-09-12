027febe8f6904ed56cc4ee2d2bb345d36c295e8a
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-12 00:01:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,35 +1,35 @@
 #require 'pry'
 
-def line(array)
-  new_array = []
-  if array.length == 0
-     puts "The line is currently empty."
-   else
-     number = array.index + 1
-     array.each do |name, number|
-     array << "#{number}"
-     array << "The line is currently: "
-     new_array << array.join("(#{number} += 1)")
-  end
-end
-new_array
-end
+# def line(array)
+#   new_array = []
+#   if array.length == 0
+#      puts "The line is currently empty."
+#    else
+#      number = array.index + 1
+#      array.each do |name, number|
+#      array << "#{number}"
+#      array << "The line is currently: "
+#      new_array << array.join("(#{number} += 1)")
+#   end
+# end
+# new_array
+# end
 
 #not passing yet - only puts first in line, not entire line
-# def line(array)
-#   deli_line = []
-#   if array.length == 0
-#     puts "The line is currently empty."
-# else
-#   deli_line << "The line is currently: "
-#     array.each do |name, number|
-#     number = array.index + 1
-#       line = "#{number}. #{name}"
-#       deli_line << "#{line}"
-# end
-#     end
-#     return deli_line
-#   end
+def line(array)
+  deli_line = []
+  if array.length == 0
+    puts "The line is currently empty."
+else
+  deli_line << "The line is currently: "
+    array.each do |name, number|
+    number = array.index += 1
+      line = "#{number}. #{name}"
+      deli_line << "#{line}"
+end
+    end
+    return deli_line
+  end
 
 
 
