abc2f5e6659d09639faa76633f020d92a3920ddc
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-12 00:25:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,7 +25,7 @@ else
       deli_line << "#{number}. #{name}"
       phrase = "The line is currently: "
       deli_line.each do |name|
-         phrase + "#{name}"
+         puts phrase + "#{name}"
 end
     end
     return deli_line
