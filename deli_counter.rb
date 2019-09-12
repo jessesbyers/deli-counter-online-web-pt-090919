0e95a64e1eacafd8dbbd32bfffa47eab38820e2b
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-12 01:33:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,6 +24,6 @@ def now_serving(array)
     puts "There is nobody waiting to be served!"
   else
     name = array.shift
-    puts "Currently serving #{first}"
+    puts "Currently serving #{first}."
   end
 end
