60344ea92a7031df2f661a2e701cc752ba5b406c
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-12 01:24:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,4 +19,5 @@ def take_a_number(array, string)
    number = index + 1
    puts "Welcome, #{name}. You are number #{number} in line."
 end
+puts "Welcome, #{array[-1]}. You are number #{number} in line.
 end
